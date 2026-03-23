022ff7b0934160a8b1f8f8839227ea3767f763da
Prajjwal Rawat <139827569+prajjwal-17@users.noreply.github.com>
2026-03-24 02:39:49 +0530

    Add NextJS and Prisma ORM to skills list

--- frontend/src/utils/skills.jsx
+++ frontend/src/utils/skills.jsx
@@ -2,9 +2,9 @@
 
 const skills = {
   programmingLanguages: ["JavaScript (ES6+)", "Python", "C++", "Java"],
-  frontend: ["React.js", "HTML5", "CSS3", "Tailwind CSS"],
+  frontend: ["React.js", "HTML5", "CSS3", "Tailwind CSS","NextJS"],
   backend: ["Node.js", "Express.js"],
-  databases: ["MongoDB", "MySQL","Neo4j"],
+  databases: ["MongoDB", "MySQL","Neo4j","Prisma ORM"],
   toolsAndPlatforms: ["Git & GitHub", "VS Code", "Postman", "Firebase", "Render"],
   concepts: ["DSA", "REST API Design", "OOP", "Responsive Web Design","Web Scraping"]
 };
